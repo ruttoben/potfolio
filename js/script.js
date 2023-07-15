3f3991ca32e6bf763737631327d6d4528a73f0a5
ruttoben <benkier08@gmail.com>
2023-07-15 21:47:24 +0300

    On branch master 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".heading").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 90%",
+    end: "bottom 10%",
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.35,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.35,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
+
+
+
+
+
 let menu =document.querySelector('#menu-bars');
 let header =document.querySelector('header')
 
@@ -49,6 +95,7 @@ var typed =  new Typed("#text1",{
     loop: false
 
 });
+
 
 
 
